7dfd54603f620bf18fed8bda5603ed0b57e4a07e
Andrey Miskov <amiskov@gmail.com>
2023-09-18 10:22:57 +0300

    add places with images

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Place, PlaceImage
 
-# Register your models here.
+
+class PlaceImageAdminInline(admin.TabularInline):
+    model = PlaceImage
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = (PlaceImageAdminInline,)
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    ...
--- places/models.py
+++ places/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('Название', max_length=128)
+    description_short = models.CharField('Короткое описчание', max_length=256)
+    description_long = models.TextField('Полное описание')
+    lat = models.DecimalField('Широта (lat)', max_digits=16, decimal_places=14)
+    lon = models.DecimalField('Долгота (lon)', max_digits=16,
+                              decimal_places=14)
+
+    def __str__(self):
+        return self.title
+
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(
+        Place, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField('Картинка')
+    position = models.IntegerField('Позиция')
+
+    def __str__(self):
+        return f'Картинка {self.position} для {self.place.title}'
