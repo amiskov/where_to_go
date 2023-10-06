6506ef526aeafa48b627b8f10331664e575d244a
Andrey Miskov <amiskov@gmail.com>
2023-10-06 21:12:33 +0300

    add logging for prod

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -141,3 +141,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+if not DEBUG:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "WARNING",
+        },
+    }
